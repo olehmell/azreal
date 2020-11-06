72a08c434380b1abc595c85d26067be32ab7032c
olehmell <oleh.melnechyk@gmail.com>
2020-11-06 22:55:06 +0200

    Improve eslinter

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -88,6 +88,25 @@ module.exports = {
 
     // We're using TypeScript, so prop-types aren't so interesting
     'react/prop-types': 'off',
+        'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [ 'off' ],
+    'semi': 'off',
+    'comma-spacing': 'warn',
+    'array-bracket-spacing': [ 'warn', 'always' ],
+    'react/display-name': 'off',
+    'padded-blocks': 'off',
+    'prefer-promise-reject-errors': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/indent': [ 'warn', 2 ],
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/array-type': 'off',
 
     // This is documented as the default, but apparently now needs to be
     // set explicitly
