d47dd5f8856bd0165742ef5659fe2fc78aa27b10
olehmell <oleh.melnechyk@gmail.com>
2020-11-08 21:35:49 +0200

    Add graphql query for measurement

--- ui/src/graphql/query/measurement/types/GetMeasuremetLastDay.ts
+++ ui/src/graphql/query/measurement/types/GetMeasuremetLastDay.ts
@@ -7,15 +7,10 @@
 // GraphQL query operation: GetMeasuremetLastDay
 // ====================================================
 
-export type Value = {
-  name: string,
-  value: number
-}
-
 export interface GetMeasuremetLastDay_az_measurements_Measurements_aggregate_nodes {
   __typename: "az_measurements_Measurements";
-  timestamp: Date;
-  values: Value[];
+  timestamp: any;
+  values: any;
   sensorId: number;
   locationId: number;
 }
