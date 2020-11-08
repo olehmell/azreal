2aa855bf1737d99fd0a79d19ad85b40f40d5bde7
olehmell <oleh.melnechyk@gmail.com>
2020-11-08 18:53:55 +0200

    Improve factors page

--- ui/src/components/factors/Factors.tsx
+++ ui/src/components/factors/Factors.tsx
@@ -1,6 +1,7 @@
 import { EuiDataGrid, EuiDataGridColumn, EuiLink, EuiLoadingChart, EuiPage, EuiPageBody, EuiPageContent, EuiPageContentBody, EuiPageHeader, EuiPageHeaderSection, EuiTitle } from '@elastic/eui';
 import React, { Fragment, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import { useGetFactors } from 'src/graphql/query/factors/getFactorsWithSensors';
+import { DataGrid } from '../utils/DataGrid';
 import { Loading } from '../utils/loading';
 import { Page } from '../utils/Page';
 
@@ -11,27 +12,26 @@ type Column = {
   amount: number
 }
 
-const DEFAULT_PAGINATION_SIZE = 20
-const columns: EuiDataGridColumn[] = [{
-    id: 'label',
-    display: 'Назва фактору',
-  },
-  {
-    id: 'unit',
-    display: 'Одиниці',
-  },
-  {
-    id: 'maxValues',
-    display: 'Максимальні значення',
-  },
-  {
-    id: 'amount',
-    display: 'Кількість датчиків',
-  }
-];
-
 const Factors = () => {
   const { data, loading, error } = useGetFactors()
+
+  const columns: EuiDataGridColumn[] = [{
+    id: 'label',
+    display: 'Назва фактору',
+    },
+    {
+      id: 'unit',
+      display: 'Одиниці',
+    },
+    {
+      id: 'maxValues',
+      display: 'Максимальні значення',
+    },
+    {
+      id: 'amount',
+      display: 'Кількість датчиків',
+    }
+  ];
 
   const sensorsData: Column[] = data?.az_sensors_PollutionFactors_aggregate.nodes
     .map(({ label, maxValues, unit, SensorFactors }) => ({
@@ -41,84 +41,13 @@ const Factors = () => {
       amount: SensorFactors.length
     }))
 
-  console.log('sensorsData', sensorsData, columns)
-
   if (error) return null;
 
   if (loading) return <Loading />
 
-  return <ViewFactors data={sensorsData} />
+  return <Page title='Вимірювальні фактори'>
+    <DataGrid data={sensorsData} columns={columns} />
+  </Page>
 }
 
-type ViewFactorsProps = {
-  data: Column[]
-}
-
-export const ViewFactors = ({ data }: ViewFactorsProps) => {
-  
-    // ** Pagination config
-    const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: data.length < DEFAULT_PAGINATION_SIZE ? data.length : DEFAULT_PAGINATION_SIZE});
-    const onChangeItemsPerPage = useCallback(
-      (pageSize) =>
-        setPagination((pagination) => ({
-          ...pagination,
-          pageSize,
-          pageIndex: 0,
-        })),
-      [setPagination]
-    );
-    const onChangePage = useCallback(
-      (pageIndex) =>
-        setPagination((pagination) => ({ ...pagination, pageIndex })),
-      [setPagination]
-    );
-  
-    // ** Sorting config
-    const [sortingColumns, setSortingColumns] = useState([]);
-    const onSort = useCallback(
-      (sortingColumns) => {
-        setSortingColumns(sortingColumns);
-      },
-      [setSortingColumns]
-    );
-  
-    // Column visibility
-    const [visibleColumns, setVisibleColumns] = useState(() =>
-      columns.map(({ id }) => id)
-    ); // initialize to the full set of columns
-  
-    const renderCellValue = useMemo(() => {
-      return ({ rowIndex, columnId }) => {
-        console.log('columnId', columnId, data[rowIndex])
-        return data.hasOwnProperty(rowIndex)
-          ? data[rowIndex][columnId]
-          : null;
-      };
-    }, []);
-  
-    return (
-      <Page
-        title='Вимірювальні фактори'
-      >
-        <EuiDataGrid
-          aria-label="Data grid demo"
-          columns={columns}
-          columnVisibility={{ visibleColumns, setVisibleColumns }}
-          rowCount={data.length}
-          renderCellValue={renderCellValue}
-          inMemory={{ level: 'sorting' }}
-          sorting={{ columns: sortingColumns, onSort }}
-          pagination={{
-            ...pagination,
-            pageSizeOptions: [20, 50, 100],
-            onChangeItemsPerPage: onChangeItemsPerPage,
-            onChangePage: onChangePage,
-          }}
-          onColumnResize={(eventData) => {
-            console.log(eventData);
-          }}
-        />
-      </Page>
-    );
-  };
 export default Factors
