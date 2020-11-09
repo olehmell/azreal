4c29190688360a7457fce8473fa9961684bf8cd5
olehmell <oleh.melnechyk@gmail.com>
2020-11-09 12:35:24 +0200

    404 page

--- ui/src/pages/404.tsx
+++ ui/src/pages/404.tsx
@@ -1,24 +1,3 @@
-import React, { Fragment, FunctionComponent } from 'react';
-import { EuiButton, EuiEmptyPrompt } from '@elastic/eui';
-import Link from 'next/link';
-
-const NotFoundPage: FunctionComponent = () => (
-  <EuiEmptyPrompt
-    iconType='editorStrike'
-    title={<h2>Ack! There&apos;s nothing here.</h2>}
-    body={
-      <Fragment>
-        <p>You found a page that doesn&apos;t exist.</p>
-      </Fragment>
-    }
-    actions={
-      <Link href='/'>
-        <EuiButton color='primary' fill>
-          Go Home
-        </EuiButton>
-      </Link>
-    }
-  />
-);
+import NotFoundPage from 'src/components/utils/NotFoundPage'
 
 export default NotFoundPage;
