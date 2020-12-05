333329653e7e834d3187c5bc07d1122a8c6a34f9
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-12-05 16:16:37 +0200

    Update Measurements SQL

--- postgresql/az_measurements.sql
+++ postgresql/az_measurements.sql
@@ -4,18 +4,19 @@ CREATE SCHEMA IF NOT EXISTS "az_measurements";
 
 CREATE TABLE IF NOT EXISTS "az_measurements"."Measurements"
 (
-    "sensorId"   integer,
+    "sensorId"      integer,
     FOREIGN KEY ("sensorId")
         REFERENCES "az_sensors"."Sensors" ("sensorId") MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE SET NULL,
-    "timestamp"  timestamp        NOT NULL,
-    "factorName" text             NOT NULL,
+    "locationPoint" point            NOT NULL,
+    "timestamp"     timestamp        NOT NULL,
+    "factorName"    text             NOT NULL,
     FOREIGN KEY ("factorName")
         REFERENCES "az_sensors"."PollutionFactors" ("name") MATCH FULL
         ON UPDATE CASCADE
-        ON DELETE NO ACTION,
-    "value"      double precision NOT NULL,
+        ON DELETE RESTRICT,
+    "value"         double precision NOT NULL,
 
-    PRIMARY KEY ("sensorId", "timestamp", "factorName")
+    UNIQUE ("sensorId", "timestamp", "factorName")
 );
